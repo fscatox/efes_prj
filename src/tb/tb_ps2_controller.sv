fc2e3e6a7690e7c4397824248efa855fb9d8009c
fscatox <125699817+fscatox@users.noreply.github.com>
2024-11-30 02:38:50 +0100

    Add error-free operation tb

--- src/tb/tb_ps2_controller.sv
+++ src/tb/tb_ps2_controller.sv
@@ -4,182 +4,123 @@
  * Date          : 29.11.2024
  */
 
+
+import tb_ps2_controller_pkg::*;
+
 module tb_ps2_controller;
 
 timeunit 1ns;
 timeprecision 1ns;
 
-// Clock specifications
-time host_tclk = 20ns; // 50 MHz host clock
-time dev_tclk = 50us; // 20 kHz device clock
-time dt_clks = lfsr_range(host_tclk-1); // dev_tclk is out of phase
+const time tclk = 20ns; // 50 MHz clock
+const time dt_clk = lfsr_range(tclk-1); // host/device clocks shift
 
-// PS2 Timing specifications
-time dev_t1 = lfsr_range(25,5)*1us; // from DATA transition to falling edge
-
-// PS/2 open-drain lines
-// (net continuously driven at '1 with pull strength)
-tri1 ps2_clk, ps2_dat;
-
-// Host interface
 logic clk, rst_n, en;
 logic tx_rqst, valid;
 logic [7:0] tx_data, rx_data;
 ps2_pkg::flags_t flags;
 
-// Device model
-bit dev_clk;
-bit dev_ps2_clk, dev_ps2_dat;
-
 //
-// Clocks
+// PS2 bus, controller, device
 //
 
-initial begin
+ps2_if ps2_bus();
+
+ps2_controller dut (
+  .*,
+  .ps2_clk(ps2_bus.clk),
+  .ps2_dat(ps2_bus.dat)
+);
+
+Ps2Device dev;
+
+//
+// Host model
+//
+
+initial begin : host_clk_p
   clk = 0;
-  forever #(host_tclk/2) clk <= ~clk;
+  forever #(tclk/2) clk <= ~clk;
 end
 
-initial begin
-  dev_clk = 1;
-  #dt_clks;
-  forever #(dev_tclk/2) dev_clk <= ~dev_clk;
-end
-
-//
-// Device behavior
-//
-
-// open-drain buffers driven by the device logic
-assign ps2_clk = dev_ps2_clk ? 'z : '0;
-assign ps2_dat = dev_ps2_dat ? 'z : '0;
-
-// device logic
-initial begin
-  dev_ps2_dat = '1;
-  dev_ps2_clk = '1;
-
-  forever begin
-
-    @(posedge dev_clk);
-    $display("[%0t] [DEVICE] Checking the bus state", $time);
-
-    if (ps2_clk)
-      // Not inhibited
-      if (ps2_dat) begin
-        // Not a request-to-send
-        devTx();
-      end
-      else begin
-      end
-  end
-end
-
-//
-// Host logic
-//
-initial begin
+initial begin : host_logic_p
   rst_n = '1;
   en = '0;
   tx_rqst = '0;
+  tx_data = '0;
   #5;
 
   // Reset the controller (asynch)
   rst_n = '0;
-  #(host_tclk/2);
+  #(tclk/2);
 
   // Keep the bus inhibited for some clock cycles
   rst_n = '1;
-  #(2*host_tclk);
+  #(2*tclk);
 
-  // Monitor incoming transmissions
   forever begin
-    en = 1;
+
+    // Receive 10 characters
+    repeat(10) begin
+      en = 1;
+      @(posedge valid);
+      @(posedge clk);
+      if (~|flags)
+        log("Host", $sformatf("rx_data = '0b%b'", rx_data));
+      else begin
+        log("host", $sformatf("Rx failed: %s", flagsToStr(flags)));
+      end
+    end
+
+    // While receiving another character
+    repeat (5)
+      @(posedge clk);
+
+    // Request transmission
+    tx_data <= lfsr_range(2**$size(tx_data)-1);
+    tx_rqst <= '1;
+
+    // By deasserting the request, a valid pulse will be generated
+    @(posedge clk);
+    tx_rqst <= '0;
+    log("Host", $sformatf("Request-to-send: tx_data = '0b%b'", tx_data));
+
+    // tx_data can change, but must have been already latched
+    tx_data <= '0;
+
     @(posedge valid);
     @(posedge clk);
     if (~|flags)
-      $display("[%0t] [HOST] rx_data = '0b%b'", $time, rx_data);
-    else begin
-      $display("[%0t] [HOST] %s", $time, flagsToStr(flags));
-    end
+      log("Host", "Tx done");
+    else
+      log("Host", $sformatf("Tx failed: %s", flagsToStr(flags)));
+
+    // Request a new transmission
+    tx_data <= lfsr_range(2**$size(tx_data)-1);
+    tx_rqst <= '1;
+
+    // With a 'blocking' valid
+    @(posedge clk);
+    log("Host", $sformatf("Request-to-send: tx_data = '0b%b'", tx_data));
+
+    @(posedge valid);
+    @(posedge clk);
+    if (~|flags)
+      log("Host", "Tx done");
+    else
+      log("Host", $sformatf("Tx failed: %s", flagsToStr(flags)));
+
+    // After some time, unblock and repeat
+    # 1ms;
+    tx_rqst <= '0;
+
   end
+
+end : host_logic_p
+
+initial begin
+  dev = new(ps2_bus.dev, dt_clk);
+  dev.run();
 end
-
-ps2_controller dut (.*);
-
-function automatic string flagsToStr(ps2_pkg::flags_t flags);
-  unique case (1'b1)
-    flags.frame_error: return "frame error";
-    flags.parity_error: return "parity error";
-    flags.clk_timeout: return "clock timeout";
-    flags.rqst_timeout: return "request-to-send timeout";
-  endcase
-endfunction
-
-function automatic int unsigned lfsr();
-  // any non-zero seed
-  static bit [31:0] state = 32'hAE1F_B42C;
-
-  // XNOR Taps: 32,22,2,1
-  // https://docs.amd.com/v/u/en-US/xapp052
-  state = {state[30:0], ~^{state[31],state[21],state[1],state[0]}};
-
-  // $display("[%0t] [LFSR] %0d", $time, state);
-  return state;
-endfunction
-
-function automatic int unsigned lfsr_range(int unsigned maxval, int unsigned minval = 0);
-  int unsigned ret = minval + (lfsr() % (maxval-minval+1));
-  // $display("[%0t] [LFSR_RANGE] %0d <= %0d <= %0d", $time, minval, ret, maxval);
-  return ret;
-endfunction
-
-// See: IBM PS/2 Reference Manuals, Keyboard/Auxiliary Device Controller, 1990
-task automatic devTx();
-  typedef bit [7:0] payload_t;
-  payload_t payload = payload_t'(lfsr_range(2**8-1));
-
-  bit [10:0] packet = {
-    1'b0, // start bit
-    {<<{payload}}, // payload lsb-first
-    ~^payload, // odd-parity
-    1'b1 // stop bit
-  };
-
-  if (lfsr_range(1,0)) begin
-    $display("[%0t] [DEVICE] Bus idle: skipping tx", $time);
-    return;
-  end
-
-  $display("[%0t] [DEVICE] Bus idle: sending '0b%b' as '0b%b'",
-    $time, payload, packet);
-
-  // Starting at a dev_clk posedge
-  foreach (packet[i]) begin
-
-    // Generate bit 'dev_t1' before the falling edge
-    #(dev_tclk/2-dev_t1);
-    $display("[%0t] [DEVICE] ps2_dat <= %b (i = %0d)", $time, packet[i], i);
-    dev_ps2_dat <= packet[i];
-
-    // Check for inhibit before generating a ps2_clk falling edge
-    @(negedge dev_clk);
-    if (!ps2_clk) begin
-      $display("[%0t] [DEVICE] Clock collision", $time);
-      dev_ps2_dat <= '1;
-      dev_ps2_clk <= '1;
-      return;
-    end
-
-    // Generate the ps2_clk falling edge
-    dev_ps2_clk <= '0;
-
-    // Generate the ps2_clk rising edge
-    @(posedge dev_clk);
-    dev_ps2_clk <= '1;
-
-  end
-
-endtask
 
 endmodule
---
+++ src/tb/tb_ps2_controller_pkg.sv
@@ -0,0 +1,192 @@
+/**
+ * File          : tb_ps2_controller_pkg.sv
+ * Author        : Fabio Scatozza <s315216@studenti.polito.it>
+ * Date          : 29.11.2024
+ */
+
+interface ps2_if;
+  tri1 clk, dat;
+  bit clk_od, dat_od;
+
+  // device drivers
+  assign clk = clk_od ? 'z : '0;
+  assign dat = dat_od ? 'z : '0;
+
+  modport dev (
+    input clk, dat,
+    output clk_od, dat_od
+  );
+
+endinterface
+
+package tb_ps2_controller_pkg;
+
+  timeunit 1ns;
+  timeprecision 1ns;
+
+  //
+  // Randomization utilities
+  //
+
+  function automatic int unsigned lfsr();
+    // any non-zero seed
+    static bit [31:0] state = 32'hAE1F_B42C;
+
+    // XNOR Taps: 32,22,2,1
+    // https://docs.amd.com/v/u/en-US/xapp052
+    state = {state[30:0], ~^{state[31],state[21],state[1],state[0]}};
+    log("LFSR", $sformatf("state = %0d", state));
+
+    return state;
+  endfunction
+
+  function automatic int unsigned lfsr_range(int unsigned maxval, int unsigned minval = 0);
+    return minval + (lfsr() % (maxval-minval+1));
+  endfunction
+
+  //
+  // Logging utilities
+  //
+
+  function automatic void log(string who, string what);
+    $display("[%0t] [%s] %s", $time, who, what);
+  endfunction
+
+  function automatic string flagsToStr(ps2_pkg::flags_t flags);
+    unique case (1'b1)
+      flags.frame_error: return "frame error";
+      flags.parity_error: return "parity error";
+      flags.clk_timeout: return "clock timeout";
+      flags.rqst_timeout: return "request-to-send timeout";
+    endcase
+  endfunction
+
+  //
+  // Device model
+  // See: IBM PS/2 Reference Manuals, Keyboard/Auxiliary Device Controller, 1990
+  //
+
+  class Ps2Device;
+
+    const time tclk;
+    const time ts; // from data transition to falling edge
+    const time dt; // clock shift
+
+    typedef bit [7:0] payload_t;
+
+    virtual ps2_if.dev ps2;
+    bit clk;
+
+    function new(virtual ps2_if.dev _ps2, time _dt = 0);
+      tclk = 50us;
+      ts = lfsr_range(25,5)*1us;
+      dt = _dt;
+
+      ps2 = _ps2;
+      clk = '0;
+    endfunction
+
+    task run();
+      ps2.dat_od = '1;
+      ps2.clk_od = '1;
+
+      fork
+
+        begin : clk_p
+          #dt;
+          forever #(tclk/2) clk <= ~clk;
+        end
+
+        forever begin
+          @(posedge clk);
+          log("Ps2Device", "Checking the bus state");
+
+          if (ps2.clk) // not inhibited
+            if (ps2.dat) // not a request-to-send
+              _tx();
+            else
+              _rx();
+        end
+
+      join
+    endtask
+
+    task _tx();
+      payload_t py = payload_t'(lfsr_range(2**8-1));
+
+      bit [10:0] pkt = {
+        1'b0, // start bit
+        {<<{py}}, // payload lsb-first
+        ~^py, // odd-parity
+        1'b1 // stop bit
+      };
+
+      log("Ps2Device", $sformatf("Sending '0b%b' as '0b%b'", py, pkt));
+      foreach (pkt[i]) begin
+
+        // Generate bit 't1' before the falling edge
+        #(tclk/2-ts);
+        ps2.dat_od <= pkt[i];
+        log("Ps2Device", $sformatf("(i = %0d) ps2_dat <= %b", i, pkt[i]));
+
+        // Check for inhibit at every falling edge, before generating
+        // a transition on the bus
+        @(negedge clk);
+        if (!ps2.clk) begin
+          log("Ps2Device", "Clock collision: aborting tx");
+          ps2.dat_od <= '1;
+          return;
+        end
+
+        // Generate the falling edge
+        ps2.clk_od <= '0;
+
+        // Generate the rising edge
+        @(posedge clk);
+        ps2.clk_od <= '1;
+
+      end
+    endtask
+
+    task _rx();
+      payload_t py;
+
+      for (int i = 0; i < 11; i++) begin
+
+        // Move to the sampling point
+        #(tclk/4);
+
+        if (i > 0 & i < 9) // data bit
+          py[i-1] = ps2.dat;
+        else if (i == 9 & ps2.dat != ~^py) begin
+          log("Ps2Device", "Host parity error");
+          $stop;
+        end
+        else if (i == 10 & !ps2.dat) begin
+          log("Ps2Device", "Host frame error");
+          $stop;
+        end
+
+        // Reverse roles for generating the acknowledgement
+        if (i == 10) begin
+          ps2.dat_od <= '0;
+        end
+
+        // Generate the falling edge
+        @(negedge clk);
+        ps2.clk_od <= '0;
+
+        // Generate the rising edge
+        @(posedge clk);
+        ps2.clk_od <= '1;
+
+      end
+
+      // Revert acknowledgement
+      ps2.dat_od <= '1;
+      log("Ps2Device", $sformatf("Rx done: '0b%b'", py));
+    endtask
+
+  endclass
+
+endpackage
